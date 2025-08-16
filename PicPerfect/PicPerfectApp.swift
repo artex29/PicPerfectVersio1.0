4824838cd70b5fd85bdefffe205e487a2d8778b1
ANGEL RAMIREZ <angelramirez@artexcomputer.net>
2025-08-16 14:36:22 -0400

    Initial Commit

---
+++ PicPerfect/PicPerfectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicPerfectApp.swift
+//  PicPerfect
+//
+//  Created by ANGEL RAMIREZ on 8/16/25.
+//
+
+import SwiftUI
+
+@main
+struct PicPerfectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
